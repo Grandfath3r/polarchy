8ea309c9612ee4eefcdcb7aa25361e32d0b0caf1
Grandfath3r <johnrobson0626@gmail.com>
2026-02-25 16:01:34 +0100

    [WIP]

--- install/packaging/nvim.sh
+++ install/packaging/nvim.sh
@@ -1,2 +1,10 @@
-# Includes lazyvim and the themes
-polarchy-nvim-setup
+#!/bin/sh
+# Install LazyVim + themes
+
+LAZYVIM="$HOME/.local/share/nvim/lazyvim"
+rm -rf "$HOME/.local/share/nvim" "$HOME/.config/nvim"
+
+git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim/.git"
+
+nvim --headless -c 'autocmd User LazySync quitall' -c 'Lazy sync' -c 'qa'
