2d62a7d1b8c2109fbcc15db142b9e1787d555f71
Grandfath3r <johnrobson0626@gmail.com>
2026-02-25 15:30:31 +0100

    [WIP]

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,4 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$POLARCHY_INSTALL/polarchy-base.packages" | grep -v '^$')
-polarchy-pkg-add yay -S --noconfirm --needed "${packages[@]}"
+polarchy-pkg-add "${packages[@]}"
+#sudo yay -S --noconfirm --needed "${packages[@]}"
