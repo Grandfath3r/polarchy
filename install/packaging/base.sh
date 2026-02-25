fbed976ea3888aec661a68d297c796d46f996433
Grandfath3r <johnrobson0626@gmail.com>
2026-02-25 15:44:42 +0100

    Added AUR Splitting

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,4 +1,13 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$POLARCHY_INSTALL/polarchy-base.packages" | grep -v '^$')
-polarchy-pkg-add "${packages[@]}"
-#sudo yay -S --noconfirm --needed "${packages[@]}"
+sudo pacman -S --noconfirm --needed "${packages[@]}"
+
+for pkg in "${all_packages[@]}"; do
+    if pacman -Si "$pkg" >/dev/null 2>&1; then
+        # Official repo: use pacman
+        sudo pacman -S --noconfirm --needed "$pkg"
+    else
+        # AUR: use yay
+        yay -S --noconfirm --needed "$pkg"
+    fi
+done
