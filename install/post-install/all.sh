4cb3f6d9873ba1973f72add6d6d1611647e92916
Grandfath3r <johnrobson0626@gmail.com>
2026-02-26 09:46:07 +0100

    [WIP]

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,4 +1,4 @@
 run_logged $POLARCHY_INSTALL/post-install/pacman.sh
 source $POLARCHY_INSTALL/post-install/allow-reboot.sh
-run_logged $POLARCHY_INSTALL/post-install/secure-boot.sh
+run_logged sudo $POLARCHY_INSTALL/post-install/secure-boot.sh
 source $POLARCHY_INSTALL/post-install/finished.sh
