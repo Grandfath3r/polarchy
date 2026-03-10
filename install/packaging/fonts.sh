f64d08d9b333b201c81ae85d85b5a8209d71b78b
Grandfath3r <johnrobson0626@gmail.com>
2026-03-10 13:26:44 +0100

    [WIP]

--- install/packaging/fonts.sh
+++ install/packaging/fonts.sh
@@ -1,4 +1,8 @@
 # Polarchy logo in a font for Waybar use
 mkdir -p ~/.local/share/fonts
 cp ~/.local/share/polarchy/config/polarchy.ttf ~/.local/share/fonts/
-fc-cache
+if command -v fc-cache >/dev/null; then
+  fc-cache -fv
+else
+  echo "fontconfig not available; skipping font cache update (fonts will load next login)"
+fi
