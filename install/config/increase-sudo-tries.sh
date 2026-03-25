f8a5c0ceca5902ec2d49ab75b19a22453c19de53
Grandfath3r <johnrobson0626@gmail.com>
2026-03-25 10:43:08 +0100

    [WIP]

--- install/config/increase-sudo-tries.sh
+++ install/config/increase-sudo-tries.sh
@@ -1,4 +1,4 @@
-# Give the user 10 instead of 3 tries to fat finger their password before lockout
+# Give the user 10 instead of 3 tries to fuck up their password before lockout
 echo "Defaults passwd_tries=10" | sudo tee /etc/sudoers.d/passwd-tries
 sudo chmod 440 /etc/sudoers.d/passwd-tries
 
